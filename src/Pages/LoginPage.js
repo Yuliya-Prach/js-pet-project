da6a929199281afbefef8cc63b8474f4acf5fc0a
Yuliya Prach <Yuliya.Prach@externals.adidas.com>
2019-08-27 16:03:37 +0300

    Created PrivateRoute, added submit button on Login page

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles(theme => ({
     textField: {
@@ -36,6 +37,9 @@ export const LoginPage = (props) => {
                     value={user}
                     onChange={handleInputChange}
                 />
+                <Button variant="outlined" color="primary" className={classes.button} type="submit">
+                    Login
+                </Button>
             </form>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, BrowserRouter as Router } from 'react-router-dom'
+import { Route, BrowserRouter as Router, Redirect } from 'react-router-dom'
 import './index.css';
 import Home from './Pages/HomePage';
 import {LoginPage} from './Pages/LoginPage';
 import {AccountPage} from './Pages/AccountPage';
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={(props) => (
+        localStorage.getItem("user") === null
+        ? <Redirect to='/login' />
+        : <Component {...props} />
+    )} />
+);
 
 const routing = (
     <Router>
         <div>
             <Route exact path="/" component={Home} />
             <Route path="/login" component={LoginPage} />
-            <Route path="/account" component={AccountPage} />
+            <PrivateRoute path="/account" component={AccountPage} />
         </div>
     </Router>
 );
